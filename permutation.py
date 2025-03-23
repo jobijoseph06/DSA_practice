39a707bba3ebd820f2ab37650e36582487a5fa50
jobi joseph <jobijoseph16@gmail.com>
2025-03-23 11:23:09 +0530

    permutation

---
+++ permutation.py
@@ -0,0 +1,12 @@
+arr = [3,2,1]
+n = len(arr)
+i = n-2
+while i>=0 and arr[i] > arr[i+1]:
+    i-=1
+if i>=0:
+    j = n-1
+    while arr[j] < arr[i]:
+        j-=1
+    arr[i],arr[j] = arr[j],arr[i]
+arr[i+1:] = reversed(arr[i+1:])
+print(arr)
