018876ad76b3efb42493d3479b1755aa1cdff5a9
jobi joseph <jobijoseph16@gmail.com>
2025-03-29 22:08:10 +0530

    subarray sum = 0

---
+++ subarray_sum 0.py
@@ -0,0 +1,16 @@
+arr =[15, -2, 2, -8, 1, 7, 10, 23]
+n = len(arr)
+maxi = 0
+s=e=0
+for i in range(n):
+    sum = 0
+    for j in range(i, n):
+        sum += arr[j]
+        if sum == 0:
+            if maxi < j-i+1:
+                maxi = j-i+1
+                s = i
+                e = j
+print(maxi)
+print(arr[s:e+1])
+
