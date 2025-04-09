acc368baf779163f1ef4ebbd9852018ea166d2cb
jobi joseph <jobijoseph16@gmail.com>
2025-04-09 20:55:07 +0530

    overlapping

--- overlapping.py
+++ overlapping.py
@@ -1,10 +1,12 @@
 def overlapping(arr):
    og = []
+   arr.sort()
    for interval in arr:
        if not og or og[-1][1] < interval[0]:
            og.append(interval)
        else:
            og[-1][1] = max(og[-1][1], interval[1])
+           og[-1][0] = min(og[-1][0], interval[0])
    return og
 
-print(overlapping([[1,3],[2,6],[8,10],[15,18]]))
+print(overlapping([[1,4],[0,0]]))
