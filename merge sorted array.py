2996a9575e6fe22d9e8a27de7ac38a59aca0ae8d
jobi joseph <jobijoseph16@gmail.com>
2025-04-10 20:29:49 +0530

    merge sorted array

--- merge sorted array.py
+++ merge sorted array.py
@@ -1,34 +1,31 @@
-def merge(arr1, arr2, m, n):
-    total = m+n
-    arr3 = [0] * total
-    left = 0
-    right = 0
-    index = 0
-    while left < m and right < n:
-        if arr1[left] < arr2[right]:
-            arr3[index] = arr1[left]
-            index+=1
-            left+=1
-        else:
-            arr3[index] = arr2[right]
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
+        total = m + n
+        nums3 = [0] * total
+        left = 0
+        right = 0
+        index = 0
+
+        while left < m and right < n:
+            if nums1[left] < nums2[right]:
+                nums3[index] = nums1[left]
+                index += 1
+                left += 1
+            else:
+                nums3[index] = nums2[right]
+                index += 1
+                right += 1
+
+        while left < m:
+            nums3[index] = nums1[left]
             index += 1
-            right+=1
-    while left < m:
-        arr3[index] = arr1[left]
-        index+=1
-    while right < n:
-        arr3[index] = arr2[right]
-        index+=1
-        right+=1
+            left += 1
 
+        while right < n:
+            nums3[index] = nums2[right]
+            index += 1
+            right += 1
 
-
-
-    print(arr1)
-    print(arr2)
-    return arr3
-
-
-
-
-print(merge([1,2,3,0,0,0], [2,5,6], 3, 3))
+        return nums3
+obj = Solution()
+print(obj.merge([1,2,3,0,0,0], 3, [2,5,6], 3 ))
