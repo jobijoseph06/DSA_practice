185a6de5fe08d84e93ea070147ea85d2fa1506ef
jobi joseph <jobijoseph16@gmail.com>
2025-04-15 20:26:01 +0530

    repeating and missing number in an array

--- missing_and_repeating_number.py
+++ missing_and_repeating_number.py
@@ -1,20 +1,42 @@
-def repeating(nums):
-   freq = []
-   n = len(nums)
-   for i in range(n):
-       freq.append(0)
+def repeating(arr):
+    freq = [0] * len(arr)
+    print(freq)
+    for num in arr:
+        freq[num-1] +=1
+    repeat = 0
+    missing = 0
+    for i in range(len(freq)):
+        if freq[i] == 0:
+            missing = i+1
+        if freq[i] == 2:
+            repeat = i+1
+    return [repeat, missing]
+print(repeating([1,2,3,6,7,5,7]))
 
-   for i in range(n):
-       num = nums[i]
-       freq[num-1] +=1
-   print(freq)
 
-   repeat = 0
-   missing = 0
-   for i in range(n):
-       if freq[i] == 2:
-           repeat = i+1
-       if freq[i] == 0:
-           missing = i+1
-   return [repeat, missing]
-print(repeating([1, 2, 3, 6, 7, 5, 7]))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
